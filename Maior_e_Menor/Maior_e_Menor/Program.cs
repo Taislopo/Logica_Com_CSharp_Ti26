16d4c413914aa42437052c9c7902f1012706aca8
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-26 11:53:40 -0300

    Maior_e_menor

---
+++ Maior_e_Menor/Maior_e_Menor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOMA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+   
+               Console.Write(" Digite um numero: ");
+               num1 = int.Parse(Console.ReadLine());
+               Console.Write(" Digite um segundo numero: ");
+               num2 = int.Parse(Console.ReadLine());
+               if (num1 >= num2)
+               { Console.Write(" o numero ", num1, " é maior que o numero ", num2);
+                    ,}
+                  else
+       {
+                    Console.Write(" o numero ", num1, " é menor que o numero ", num2);
+                    }
+       
+
+
+
+
+        }
+    }
+}
