19036c6da67f7aa12dff0ca48366601e3494aa31
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-26 11:22:21 -0300

    Peso
    Ver quanto passa no kg e a multa

---
+++ Peso_Peixe/Peso_Peixe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peso_Peixe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Double: real
+            Double peso, sobra;
+             
+             Console.Write(" Digite o peso pescado: ");
+             peso = Double.Parse(Console.ReadLine());
+             sobra = (peso - 50) * 4;
+             Console.Write(sobra);
+  }
+
+
+
+
+
+
+
+
+    }
+    }
+
