0ec4f0e4e46ae71538d1196934a960fac9bc7de1
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-27 09:04:50 -0300

    Prefeitura

---
+++ Prefeitura/Prefeitura/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prefeitura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, prestacao, limite;
+
+            Console.Write(" Qual é o seu salario? ");
+            salario = double.Parse(Console.ReadLine());
+            Console.Write(" Qual é o valor da prestação? ");
+            prestacao = Double.Parse(Console.ReadLine());
+            limite = salario * 0.30;
+            if(prestacao >= limite)
+    {
+                Console.Write(" O emprestimo não poderá ser concedido ");
+                    }
+            else
+    {
+                Console.Write(" O emprestimo será concendido ");
+                    }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
