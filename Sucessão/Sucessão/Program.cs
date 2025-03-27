2acb006f94829c80d40bb4092cce53df17163924
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-27 09:05:36 -0300

    sucessão

---
+++ Sucessão/Sucessão/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sucessão
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, soma, menos;
+
+            Console.Write(" Digite um numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            menos = num1 - 1;
+            soma = num1 + 1;
+            Console.WriteLine(" o antecessesor do numero " + num1 + " é o " + menos + " e o sucessor é o " + soma);
+
+
+
+
+        }
+    }
+}
