3b65d26cf25bf0d90a0c923329f4617dbf6126f3
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:05:34 -0300

    Temperatura

---
+++ temperatura/temperatura/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double temperatura;
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            temperatura = double.Parse(Console.ReadLine());    
+
+            if (temperatura < 15) {
+               Console.WriteLine("Frio ");
+    }
+             else if(temperatura >= 15 && temperatura <= 25) {
+                 Console.WriteLine("Ameno ");
+            }
+              else {
+                  Console.WriteLine("Quente ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
