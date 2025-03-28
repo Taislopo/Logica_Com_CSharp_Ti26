dd29d2d49484ab8a70f0d9ade37d7041bf27bb2f
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:04:09 -0300

    Para_Impares

---
+++ Para_impares/Para_impares/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Para_impares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Mostre somente os números ímpares de 1 a 100.
+
+            int contador;
+            
+          
+           for(contador = 1; contador <= 100; contador++)
+    {
+                Console.WriteLine("COTANDO.." + contador);
+                contador = contador + 2;
+    }
+
+
+
+        }
+    }
+}
