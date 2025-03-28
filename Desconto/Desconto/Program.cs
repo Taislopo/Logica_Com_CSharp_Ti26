e478292473213fbada1698d687d3e79e599f375f
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:56:15 -0300

    Desconto

---
+++ Desconto/Desconto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desconto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Um produto com preço acima de 100 reais tem um desconto de 10%. Caso contrário,
+            //o desconto é de 5%. Dado o preço, calcule o valor do desconto programa
+
+            double valor, desconto1;
+
+
+            Console.Write(" Digite um valor: ");
+            valor = double.Parse(Console.ReadLine());
+            if(valor >= 100)
+  {
+             desconto1 = valor * 0.1;
+             Console.Write(" Você recebeu um desconto de 10% que é " + desconto1);
+  }
+            else
+   {
+               desconto1 = valor * 0.05;
+               Console.Write("Você recebeu um desconto de 5% que é " + desconto1);
+   }
+        }
+    }
+    }
+
