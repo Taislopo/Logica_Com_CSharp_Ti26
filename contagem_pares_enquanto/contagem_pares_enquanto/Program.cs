4b96ee4bd73d78f6e83d45043332c77b08b07dcb
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:55:26 -0300

    Contagem_Pares_Enquanto

---
+++ contagem_pares_enquanto/contagem_pares_enquanto/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contagem_pares_enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int contagem;
+
+            contagem = 0;
+            while(contagem<100)
+            {
+               contagem = contagem + 2;
+                Console.WriteLine("Numeros pares " + contagem);
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
