0c731af0a231a59a9cc37e51b65f50efca94f70b
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:51:18 -0300

    Contador_Para

---
+++ Contador/Contador/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contador_enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+
+            while (contador <= 100)
+            {
+                if (contador % 2 == 0) {
+
+                    Console.WriteLine(contador)
+
+            }
+                contador++;
+
+            }
+        }
+    }
+}    
+
