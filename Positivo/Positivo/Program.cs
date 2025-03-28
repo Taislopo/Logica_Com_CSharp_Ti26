7af5ae198fbfe871a9280b551c7f0227ad19263b
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:05:07 -0300

    Positivo

---
+++ Positivo/Positivo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Positivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+            Console.Write(" Digite um numero: ");
+            num = int.Parse(Console.ReadLine());
+            if(num < 0)
+    {
+                Console.WriteLine(" Esse numero é negativo ");
+  }
+           else
+    {
+                Console.WriteLine(" Esse numero é positivo ");
+    }
+        }
+    }
+    }
+
