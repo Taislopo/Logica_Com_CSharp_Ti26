101fccb2335c932e940c24603b16daf723d406d6
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:03:02 -0300

    Menor

---
+++ Menor/Menor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2, num3;
+
+            Console.Write("Digite um numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite ultima numero: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 < num2 && num1<num3)
+     {
+               Console.Write(" o numero menor é " + num1);
+     }
+           else if(num2 > num1 && num2  > num3)
+     {
+                Console.Write(" o numero menor é " + num2);
+     }
+            else
+      {
+                Console.Write(" o numero menor é " + num3);
+      }
+        }
+
+
+
+
+
+
+
+
+    }
+    }
