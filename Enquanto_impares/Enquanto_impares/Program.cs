43060ad550436660f7ef20011e47ca7178123c3e
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:58:36 -0300

    Enquanto_Impares

---
+++ Enquanto_impares/Enquanto_impares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enquanto_impares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // Mostre somente os números ímpares de 1 a 100.
+
+            int contador;
+
+            contador = 1;
+            
+            while (contador <101)
+            {
+                Console.WriteLine("COTANDO.." + contador);
+                contador = contador + 2;
+            }
+
+        }
+    }
+}
