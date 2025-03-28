1b42b3322fc381128bb424991ed2c21d0a49c640
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:55:54 -0300

    Contagem_Pares_Para

---
+++ Contagem_pares_para/Contagem_pares_para/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contagem_pares_para
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pares, contagem;
+            pares = 0;
+            for(contagem = -2; contagem <= 98; contagem++)
+    {
+               pares = pares  + 2;
+                Console.WriteLine("Numeros pares " + pares);
+    }
+
+        }
+
+
+    }
+}
