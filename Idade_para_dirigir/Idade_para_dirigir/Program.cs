5aeb8577e8a512a2892b971ca390f0b8fe7c0f5f
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:59:26 -0300

    Idade_para_Dirigir

---
+++ Idade_para_dirigir/Idade_para_dirigir/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade_para_dirigir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Verifique se uma pessoa tem idade suficiente para obter a carteira de motorista,
+            //considerando que a idade mínima é 18 anos.
+
+            int idade;
+            Console.Write(" Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+            if(idade >= 18)
+     {
+                Console.Write(" Tem idade para dirigir");
+     }
+                else
+     {
+                Console.Write(" Não tem idade para dirigir ");
+     }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
