6b63771f577eb94f2f9a34de5f1937f18b7db4ef
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:59:05 -0300

    Faixa_de_Numeros

---
+++ Faixa_de_numero/Faixa_de_numero/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faixa_de_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Dado um número, verifique se ele está na faixa de 1 a 10, 11 a 20
+            //ou acima de 20 programa
+            int num;
+            Console.Write(" Digite um numero: ");
+            num = int.Parse(Console.ReadLine());
+            if(num > 0 && num < 11)
+    {
+                Console.Write(" O numero está na faixa de 1 a 10 ");
+    }
+           else        if (num> 10 && num<20)
+                Console.WriteLine(" O numero está na faixa de 11 a 20 ");
+            else
+                Console.WriteLine(" O numero está acima de 20 ");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
