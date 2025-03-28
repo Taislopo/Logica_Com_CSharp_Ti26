f794aaf77bcc1eb7dc04933d41900199eb81ff97
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:02:50 -0300

    Media

---
+++ Media_de_notas/Media_de_notas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_de_notas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcule a média de quatro notas e determine se o aluno foi aprovado ou reprovado.
+            //Considera-se aprovado quem tem média maior ou igual a 7 programa
+
+            double nota1, nota2, nota3, nota4, media;
+            Console.Write("Digite primeira nota: ");
+            nota1 = int.Parse(Console.ReadLine);
+            Console.Write(" Digite segunda nota: ");
+            nota2 = int.Parse(Console.ReadLine);
+            Console.Write(" Digite terceira nota: ");
+            nota3 = int.Parse(Console.ReadLine);
+            Console.Write(" Digite quarta nota: ");
+            nota4 = int.Parse(Console.ReadLine);
+            media = (nota1 + nota2 + nota3 + nota4) / 4)
+            if (media >= 7)
+            {
+                Console.Write("Aluno foi aprovado com a nota " + media);
+            }
+            else
+                Console.Write(" Aluno foi reprovado com a nota " + media);
+                               
+
+  }
+
+    }
+    }
+
