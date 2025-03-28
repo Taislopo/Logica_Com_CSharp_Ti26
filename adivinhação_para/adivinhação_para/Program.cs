fe6c42d2c74647d4f78b4444e5370d10bb4d1190
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:49:48 -0300

    Adivinhação_Para

---
+++ adivinhação_para/adivinhação_para/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adivinhação_para
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int numero, acertos, contador, advin;
+          
+                acertos = 0;
+                Console.Write("\n JOGADOR 1 Digite um numero de 1 a 10 \n");
+                numero = int.Parse(Console.ReadLine());
+
+                for (contador=0; contador <= 21; contador++)
+                {
+                    Console.Write(" JOGADOR 2 Adivinhe o numero escolhido \n");
+                    advin = int.Parse(Console.ReadLine());
+                    contador = contador + 1;
+                    if (advin == numero) ;
+                    {
+                        acertos = acertos + 1;
+
+                    }
+                    Console.Write(" VOCÊ ACERTOU O NUMERO " + advin + " NA TENTATIVA " + contador);
+                }
+
+            }
+        }
+    }
+}
