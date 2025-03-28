627af8b347cfb08e9cec901fe371903feeaf8e81
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:07:23 -0300

    Zero_Enquanto

---
+++ Zero_Enquanto/Zero_Enquanto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero_Enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa que pergunte sua idade e responda o ano que você
+            //nasceu. O programa deverá permanecer perguntando idades
+            //até que o usuário digite como a idade o número 0 (zero)
+
+
+            int contador, idade;
+            contador = 1;
+            while(contador == 1)
+    {
+                Console.Write("Digite sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                if(idade == 0)
+       {
+                    contador = contador + 100;
+       }
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
