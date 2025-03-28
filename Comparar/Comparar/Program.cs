2dc3471a7a1d6d88892e4c6174d77481e836987f
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:50:35 -0300

    Comparar

---
+++ Comparar/Comparar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verifique se dois números são iguais ou diferentes.
+            int num1, num2;
+            Console.Write("Digite um numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write(" Digite mais um numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if(num1 == num2)
+    {
+              Console.Write(" Esses numeros são iguais");
+    }
+             else
+               Console.Write(" Esses numeros são diferentes");
+  }
+
+
+
+
+
+
+
+
+
+    }
+    }
