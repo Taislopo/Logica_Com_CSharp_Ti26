5ffb7f27674b10809c62ebdb7eca82603da99e06
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:50:12 -0300

    Bissexto

---
+++ Bissexto/Bissexto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double ano;
+            //Determine se um ano é bissexto.
+            //Um ano é bissexto se for divisível por 4, mas não por 100, a não ser que seja divisível por 400.
+            Console.Write("digite seu ano  ");
+              ano = double.Parse(Console.ReadLine());    
+              if(ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0)
+                {
+                Console.Write("é bissexto");
+     }
+               else{
+                    Console.Write("não é bissexto");
+     }
+
+
+
+
+
+
+
+
+        }
+    }
+}
