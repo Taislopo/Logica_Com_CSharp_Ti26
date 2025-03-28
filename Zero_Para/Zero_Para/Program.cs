88f8f00be15b81217c604d984d19a9a8d16f895d
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:07:49 -0300

    Zero_Para

---
+++ Zero_Para/Zero_Para/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero_Para
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Programa que pergunte sua idade e responda o ano que você
+            //nasceu. O programa deverá permanecer perguntando idades
+            //até que o usuário digite como a idade o número 0 (zero)
+
+
+            int contador, idade;
+
+           for(contador=0; contador <= 99; contador++)
+            {
+                Console.Write("Digite sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                if (idade == 0)
+                {
+                    contador = contador + 100;
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
