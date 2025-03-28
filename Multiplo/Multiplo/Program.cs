f94154709e04f44e1226f78e485b04d496460180
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:03:18 -0300

    Multiplo

---
+++ Multiplo/Multiplo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+            Console.WriteLine("Digite um numero: ");
+    num = int.Parse(Console.ReadLine());
+               if(num % 3 == 0 && num == num % 5 == 0)
+    {
+                Console.WriteLine(" Esse numero é multiplo de 3 ou de 5");
+    }
+            else
+    {
+                Console.WriteLine((" Esse numero não é multiplo de 3 ou 5");
+    }
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
