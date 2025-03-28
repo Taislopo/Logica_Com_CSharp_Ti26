2546fd3d587950a938e82b04e523c3ffc271170c
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:56:34 -0300

    Dia_Da_Semana

---
+++ Dia_da_semana/Dia_da_semana/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia_da_semana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determine se o dia informado é um dia útil ou final de semana.
+            
+            int dia;
+            Console.Write(" Digite um número correspondente ao dia da semana ");
+            dia = int.Parse(Console.ReadLine());
+            if (dia == 6 && dia == 7)
+            {
+                Console.Write(" Final de semana ");
+            }
+            else
+                Console.Write(" Dia de semana ");
+
+
+  }
+    }
+    }
