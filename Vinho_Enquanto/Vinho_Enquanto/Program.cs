589ad2af996893987e72d1486a004c29b26eac94
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 11:06:47 -0300

    Vinho_Enquanto

---
+++ Vinho_Enquanto/Vinho_Enquanto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinho_Enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int qtdVinhoTinto = 0, qtdVinhoBranco = 0;
+            string tipoVinho;
+
+            while (tipoVinho != "f") 
+    {
+                Console.Write("Digite o tipo do vinho (t - tinto / b - branco): ");
+                tipoVinho = Console.ReadLine();
+
+
+                if(tipoVinho == "t")
+      {
+                    qtdVinhoTinto++;
+      }
+               else
+      {
+                    qtdVinhoBranco++;
+      }
+            }
+
+            Console.Write("Quantidade de vinho branco: " + qtdVinhoBranco );
+            Console.Write("Quantidade de vinho tinto: " + qtdVinhoTinto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
