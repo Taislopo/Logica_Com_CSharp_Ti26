f91ffbdcb027694842028af77a29ea726e12df83
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-03-28 10:59:56 -0300

    Keno

---
+++ KENO/KENO/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace KENO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crie um algoritmo para verificar se o Keno possui uma perna mais curta que a outra, peça a medida exata 
+            //de cada perna e verifique a metragem exibindo as seguintes possibilidades de mensagem: 
+            //Cotó na perna esquerda, cotó na perna direita ou normal.
+
+
+            Double tam1, tam2;
+
+            Console.Write(" Qual o tamanho da perna direita? ");
+            tam1 = int.Parse(Console.ReadLine());
+            Console.Write(" Qual o tamanho da perna esquerda? ");
+            tam2 = int.Parse(Console.ReadLine());
+            if(tam1 > tam2)
+     {
+                Console.Write("Cotó na perna esquerda ");
+     }
+            else
+               if(tam1 == tam2)
+      {
+                Console.Write(" NORMAL");
+      }
+            else
+             if (tam1<tam2)
+            {
+                Console.Write(" Cotó na perna direita");
+             }
+
+        }
+
+
+
+
+
+
+
+
+    }
+    }
+
